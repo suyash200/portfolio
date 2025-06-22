63c4869bd05b977e71c67c6fb829ee6f131b3b34
suyash200 <yashlade@gmail.com>
2025-06-22 16:03:14 +0530

    adding dist dir

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,9 @@ const nextConfig: NextConfig = {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
+
   },
+  distDir: "./dist"
 }
 
 export default nextConfig
